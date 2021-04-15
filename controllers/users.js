c261600b469074bc050e46897d2fda0aa78f8ed5
Bryanromerou <bryanromerou@gmail.com>
2021-04-14 20:00:47 -0700

    adds register route

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,59 @@
 const db = require("../models");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const index = (req,res)=>{
-  res.send("howdy")
+  res.send(process.env.jwtSecret)
+};
+
+const create = async (req,res)=>{
+  const {email, password, name} = req.body;
+
+  if (!name || !email || !password){
+    return res.status(400).json({ msg: 'Please enter all fields' });
+  }
+
+  try {
+    const user = await User.findOne({email});
+    if (user) throw Error('User under this email already exist');
+
+    const salt = await bcrypt.genSalt(10);
+    if (!salt) throw Error('Something went wrong with bcrypt');
+
+    const hash = await bcrypt.hash(password,salt);
+    if (!hash) throw Error('Something went wrong hashing the password');
+
+    const newUser = new db.User({
+      name,
+      email,
+      password:hash
+    });
+
+    const savedUser = await newUser.save();
+    if(!savedUser) throw Error('Something went wrong trying to save the user');
+
+    const token = jwt.sign({id: savedUser._id},process.env.jwtSecret,{ expiresIn: 86400});
+
+    res.status(200).json({
+      token,
+      user: {
+          id: savedUser.id,
+          name: savedUser.name,
+          email: savedUser.email
+      }
+    });
+
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+
+  res.send("create")
 };
 
 module.exports = {
   index,
   // show,
-  // create,
+  create,
   // update,
   // destroy,
   // findById  
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const ctrl = require("../controllers");
 
 router.get("/",ctrl.users.index);
 // router.get("/:id",ctrl.questions.show);
-// router.post("/",ctrl.questions.create);
+router.post("/",ctrl.users.create);
 // router.put("/:id",ctrl.questions.update);
 // router.delete("/:id",ctrl.questions.destroy);
 
